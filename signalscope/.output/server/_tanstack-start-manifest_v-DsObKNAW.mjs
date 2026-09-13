efdf0527bbcb60624a07076e975cdc2f1cb6bed3
Yatrik Dhameliya <126285146+Yatrik5634@users.noreply.github.com>
2026-09-13 17:59:41 +0530

    Add signalscope/.output/server/_tanstack-start-manifest_v-DsObKNAW.mjs

---
+++ signalscope/.output/server/_tanstack-start-manifest_v-DsObKNAW.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DsObKNAW.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/Yatrik/Desktop/SIH/signalscope/src/routes/__root.tsx",
+		children: ["/", "/about"],
+		preloads: ["/assets/index-CTxjxXC1.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CTxjxXC1.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/Yatrik/Desktop/SIH/signalscope/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-Bp_8i-S9.js", "/assets/SiteHeader-CDkhO6vk.js"]
+	},
+	"/about": {
+		filePath: "C:/Users/Yatrik/Desktop/SIH/signalscope/src/routes/about.tsx",
+		children: void 0,
+		preloads: ["/assets/about-5rvPWIk1.js", "/assets/SiteHeader-CDkhO6vk.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
